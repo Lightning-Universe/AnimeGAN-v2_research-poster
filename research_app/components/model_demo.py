2e2a1134f975d2cc554ff61504876df0f401dc4e
Aniket Maurya <theaniketmaurya@gmail.com>
2022-06-07 20:35:39 +0530

    auto device select

--- research_app/components/model_demo.py
+++ research_app/components/model_demo.py
@@ -2,10 +2,7 @@ import logging
 from functools import partial
 
 import gradio as gr
-import lightning as L
-import requests
 import torch
-from PIL import Image
 from lightning.components.serve import ServeGradio
 from rich.logging import RichHandler
 
@@ -33,7 +30,8 @@ class ModelDemo(ServeGradio):
         return self.model(img=img)
 
     def build_model(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
         repo = "AK391/animegan2-pytorch:main"
-        model = torch.hub.load(repo, "generator", device="cpu")
-        face2paint = torch.hub.load(repo, "face2paint", size=512, device="cpu")
+        model = torch.hub.load(repo, "generator", device=device)
+        face2paint = torch.hub.load(repo, "face2paint", size=512, device=device)
         return partial(face2paint, model=model)
