47c980a5bdee14aa97896eeded77482c021798a1
Aniket Maurya <theaniketmaurya@gmail.com>
2022-06-07 16:24:27 +0530

    update

--- research_app/components/model_demo.py
+++ research_app/components/model_demo.py
@@ -1,5 +1,12 @@
 import logging
+from functools import partial
 
+import gradio as gr
+import lightning as L
+import requests
+import torch
+from PIL import Image
+from lightning.components.serve import ServeGradio
 from rich.logging import RichHandler
 
 FORMAT = "%(message)s"
@@ -7,26 +14,20 @@ logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[Ric
 
 logger = logging.getLogger(__name__)
 
-from functools import partial
-import gradio as gr
-import torch
-from PIL import Image
-import requests
-from lightning.components.serve import ServeGradio
-
 
 # Credit to @akhaliq for his inspiring work.
 # Find his original code there: https://huggingface.co/spaces/akhaliq/AnimeGANv2/blob/main/app.py
 class ModelDemo(ServeGradio):
     inputs = gr.inputs.Image(type="pil", label="Upload to Animate your photo")
     outputs = gr.outputs.Image(type="pil", label="Animated Output")
-    elon = "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg"
-    img = Image.open(requests.get(elon, stream=True).raw)
-    img.save('elon.jpg')
+
+    # elon = "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg"
+    # img = Image.open(requests.get(elon, stream=True).raw)
+    # img.save('elon.jpg')
     # examples = [['elon.jpg']]
 
     def __init__(self):
-        super().__init__()
+        super().__init__(cloud_compute=L.CloudCompute("cpu", 1))
 
     def predict(self, img):
         return self.model(img=img)
