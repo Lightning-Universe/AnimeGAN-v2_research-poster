d3b517d5a8b204416c15297333c86a6526c5f0da
Aniket Maurya <theaniketmaurya@gmail.com>
2022-06-07 10:41:21 +0530

    Initial commit

---
+++ research_app/components/model_demo.py
@@ -0,0 +1,42 @@
+import logging
+
+import gradio as gr
+from lightning.components.serve import ServeGradio
+from rich.logging import RichHandler
+
+from research_app.clip_demo import CLIPDemo
+
+FORMAT = "%(message)s"
+logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
+
+logger = logging.getLogger(__name__)
+
+
+class ModelDemo(ServeGradio):
+    """Serve model with Gradio UI.
+
+    You need to define i. `build_model` and ii. `predict` method and Lightning `ServeGradio` component will
+    automatically launch the Gradio interface.
+    """
+
+    inputs = gr.inputs.Textbox(default="Doctor Strange Multiverse", label="Search your favourite movie here")
+    outputs = gr.outputs.HTML(label="Fetch Images from <b>themoviedb.org</b>")
+
+    def __init__(self):
+        super(ServeGradio, self).__init__(parallel=True)
+        self._model = None
+        self.enable_queue = True
+
+    def build_model(self) -> CLIPDemo:
+        logger.info("loading model...")
+        clip = CLIPDemo()
+        logger.info("built model!")
+        return clip
+
+    def predict(self, query: str) -> str:
+        return self.model.predict(query)
+
+
+if __name__ == "__main__":
+    demo = ModelDemo()
+    demo.run()
---
+++ research_app/utils.py
@@ -0,0 +1,47 @@
+import logging
+import os
+import subprocess
+from pathlib import Path
+
+from rich import print
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
+
+logger = logging.getLogger(__name__)
+
+
+def notebook_to_html(path: str):
+    """Provided notebook file path will be converted into html."""
+    command = f"jupyter nbconvert --to html {path}"
+    subprocess.run(command, shell=True)
+    folder = "/".join(path.split("/")[:-1])
+    html = path.replace("ipynb", "html")
+    os.rename(html, folder + "/index.html")
+    return folder
+
+
+def clone_repo(url: str):
+    """Clones the github repo from url to current dir.
+
+    Example:
+        url = "https://github.com/PyTorchLightning/lightning-template-research-app.git"
+        clone_repo(url)
+
+    The given repo will be cloned to current dir.
+    """
+    print(f"cloning {url}")
+    path = Path.cwd() / "github"
+    os.makedirs(path, exist_ok=True)
+    target_path = str(path / os.path.basename(url)).replace(".git", "")
+
+    if os.path.exists(target_path):
+        cmd = f"cd {target_path} && git pull"
+    else:
+        cmd = f"git clone {url} {target_path}"
+    return subprocess.run(cmd, shell=True), target_path
+
+
+if __name__ == "__main__":
+    clone_repo("https://github.com/PyTorchLightning/lightning-template-research-app.git")  # E501
